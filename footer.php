74733dcbc0469c2d8d3f22feae3d46a8ecc8229f
avronilrajib <avronilrajib@gmail.com>
2024-03-22 02:15:21 +0600

    some style add

--- footer.php
+++ footer.php
@@ -6,7 +6,7 @@
           <div class="mb-6 md:mb-0">
               <a href="index.php" class="flex items-center">
                   <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 me-3" alt="FlowBite Logo" />
-                  <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">AMS</span>
+                  <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">A M S</span>
               </a>
           </div>
           <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
