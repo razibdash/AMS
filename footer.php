e14b860dbcfaadc915ec1f968e027e1556589939
sazida81 <sanzidachowdhury81@gmail.com>
2024-05-23 09:57:09 +0600

    update footer section

--- footer.php
+++ footer.php
@@ -46,7 +46,7 @@
             </div>
             <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
             <div class="sm:flex sm:items-center sm:justify-between">
-                <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://www.facebook.com/avronilrajib" class="hover:underline">Avronil Rajib</a>. All Rights Reserved.
+                <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="https://www.facebook.com/avronilrajib" class="hover:underline">Avronil Rajib</a>. All Rights Reserved.
                 </span>
                 <div class="flex mt-4 sm:justify-center sm:mt-0">
                     <a href="https://www.facebook.com/metropolitanuniversity" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
